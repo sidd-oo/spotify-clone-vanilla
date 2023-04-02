7a19d54ec71db4a1cfd46a760c80f60bd23d9012
SIDDHARTH SAHOO <siddharth@kointrack.com>
2023-04-02 23:33:18 +0530

    feat: added profile menu and logout feat

--- src/common.js
+++ src/common.js
@@ -1,5 +1,20 @@
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
 export const TOKEN_TYPE = "TOKEN_TYPE";
 export const EXPIRES_IN = "EXPIRES_IN";
+const APP_URL = import.meta.env.VITE_APP_URL;
 
+
+export const ENDPOINT = {
+    userInfo: "me",
+    featuredPlayist: "browse/featured-playlists?limit=5",
+    toplists: "browse/categories/toplists/playlists?limit=10",
+    playlist: "playlists"
+}
+
+export const logout = () => {
+    const accessToken = localStorage.removeItem(ACCESS_TOKEN);
+    const tokenType = localStorage.removeItem(TOKEN_TYPE);
+    const expiresIn = localStorage.removeItem(EXPIRES_IN);
+    location.href = APP_URL;
+}
  
---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,38 @@
+import { fetchRequest } from "../api";
+import { ENDPOINT, logout } from "../common";
+
+const onProfileClick = (event) => {
+    event.stopPropagation();
+    const profileMenu = document.querySelector("#profile-menu");
+    profileMenu.classList.toggle("hidden");
+    if (!profileMenu.classList.contains("hidden")) {
+        profileMenu.querySelector("li#logout").addEventListener("click", logout)
+    }
+}
+
+const loadUserProfile = async () => {
+    const defaultImage = document.querySelector("#default-image");
+    const displayNameElement = document.querySelector("#display-name");
+    const profileButton = document.querySelector("#user-profile-btn");
+    const { display_name: displayName, images } = await fetchRequest(ENDPOINT.userInfo);
+
+    if (images?.length) {
+        defaultImage.classList.add("hidden");
+    } else {
+        defaultImage.classList.remove("hidden")
+    }
+
+    profileButton.addEventListener("click", onProfileClick)
+
+    displayNameElement.textContent = displayName;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadUserProfile();
+    document.addEventListener("click", () => {
+        const profileMenu = document.querySelector("#profile-menu");
+        if (!profileMenu.classList.contains("hidden")) {
+            profileMenu.classList.add("hidden");
+        }
+    })
+})
